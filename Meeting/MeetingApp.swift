4416b91205a2bbdc79492e5df60b58089776fc50
Teff <110036529+runnneverstop722@users.noreply.github.com>
2023-05-21 22:33:58 +0900

    Initial Commit

---
+++ Meeting/MeetingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingApp.swift
+//  Meeting
+//
+//  Created by Teff on 2023/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MeetingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
