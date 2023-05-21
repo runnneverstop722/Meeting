4e8fdb4fd8595f9d7d9733d630c5d15e730a6913
Teff <110036529+runnneverstop722@users.noreply.github.com>
2023-05-21 22:53:40 +0900

    - Create a color theme : To create a consistent look throughout your app, you’ll create a theme with two contrasting color properties. You’ll use the main color for a view’s background and the accent color for a view’s text.

---
+++ Meeting/Models/Theme.swift
@@ -0,0 +1,39 @@
+//
+//  Theme.swift
+//  Meeting
+//
+//  Created by Teff on 2023/05/21.
+//
+
+import SwiftUI
+
+// Add a Color property named mainColor that creates a color using the enumeration’s rawValue. This property initializes a color from the asset catalog.
+enum Theme: String {
+    case bubblegum
+    case buttercup
+    case indigo
+    case lavender
+    case magenta
+    case navy
+    case orange
+    case oxblood
+    case periwinkle
+    case poppy
+    case purple
+    case seaform
+    case sky
+    case tan
+    case teal
+    case yellow
+    
+    var accentColor: Color {
+        switch self {
+        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seaform, .sky, .tan, .teal, .yellow: return .black
+        case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+        }
+    }
+    
+    var mainColor: Color {
+        Color(rawValue)
+    }
+}
