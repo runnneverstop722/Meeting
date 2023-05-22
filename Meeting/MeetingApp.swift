4d8675cdbeb255635065688f293a77a54546436f
Teff <110036529+runnneverstop722@users.noreply.github.com>
2023-05-22 14:36:09 +0900

    - Set up navigation : From the NavigationStack container view, you can traverse a stack of views in a hierarchy.

--- Meeting/MeetingApp.swift
+++ Meeting/MeetingApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct MeetingApp: App {
     var body: some Scene {
         WindowGroup {
-            MeetingView()
+            ScrumsView(scrums: DailyScrum.sampleData)
+            // Set ScrumsView as the initial view for the app. WindowGroup is one of the primitive scenes that SwiftUI provides. In iOS, the views you add to the WindowGroup scene builder are presented in a window that fills the device’s entire screen.
         }
     }
 }
