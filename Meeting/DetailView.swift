cf8bce7885a15a5ecab8f4ba59da318a35a7e5e4
Teff <110036529+runnneverstop722@users.noreply.github.com>
2023-05-22 15:42:36 +0900

    - Add visual components to the detail view : The completed detail view displays the name of a scrum, meeting duration, card color, and attendee list.

--- Meeting/DetailView.swift
+++ Meeting/DetailView.swift
@@ -11,7 +11,29 @@ struct DetailView: View {
     let scrum: DailyScrum
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section(header: Text("Meeting Info")) {
+                Label("Start Meeting", systemImage: "timer") // Tip: By default, VoiceOver reads the title of the Label. If you want VoiceOver to convey more detail, replace the default spoken text by adding an accessibilityLabel that describes the element.
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine) //Add accessibilityElement(children:) to the HStack to combine the Label and Text elements for accessibility users. VoiceOver then reads the two elements as one statement, for example, “Length, 10 minutes.” Without the modifier, VoiceOver users have to swipe again between each element.
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+                .accessibilityElement(children: .combine)
+            }
+        }
     }
 }
 
