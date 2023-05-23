999aa137601513623ce4d04a23574a2e8cf8918b
Teff <110036529+runnneverstop722@users.noreply.github.com>
2023-05-23 17:30:58 +0900

    - Add design elements to the meeting header : The view now uses dynamic properties, but the user interface needs a few design adjustments to approach the final design.

--- Meeting/MeetingHeaderView.swift
+++ Meeting/MeetingHeaderView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct MeetingHeaderView: View {
     let secondsElapsed: Int
     let secondsRemaining: Int
+    let theme: Theme
 
     private var totalSeconds: Int {
         secondsElapsed + secondsRemaining
@@ -25,6 +26,7 @@ struct MeetingHeaderView: View {
     var body: some View {
         VStack {
         ProgressView(value: progress)
+                .progressViewStyle(ScrumProgressViewStyle(theme: theme))
             HStack {
                 VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
@@ -36,18 +38,20 @@ struct MeetingHeaderView: View {
                     Text("Seconds Remaining")
                         .font(.caption)
                     Label("\(secondsRemaining)", systemImage: "hourglass.bottomhalf.fill")
+                        .labelStyle(.trailingIcon)
                 }
             }
         }
         .accessibilityElement(children: .ignore)
         .accessibilityLabel("Time remaining")
         .accessibilityValue("\(minutesRemaining) minutes")
+        .padding([.top, .horizontal])
     }
 }
 
 struct MeetingHeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 100)
+        MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 100, theme: .bubblegum)
             .previewLayout(.sizeThatFits) // To more accurately represent the subview size in the canvas
     }
 }
