7f0eee2ff94c8fda3a8111ce3615d55f8b6f8a37
Teff <110036529+runnneverstop722@users.noreply.github.com>
2023-05-26 09:56:45 +0900

    - Add a method to load data : Add a method to read JSON data from the scrums.data file and decode the data to an array of daily scrums. Reading from the file system can be slow. To keep the interface responsive, write an asynchronous function to load data from the file system. Making the function asynchronous lets the system efficiently prioritize updating the user interface instead of sitting idle and waiting while the file system reads data.

--- Meeting/Models/ScrumStore.swift
+++ Meeting/Models/ScrumStore.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+@MainActor // The class must be marked as @MainActor before it is safe to update the published scrums property from the asynchronous load() method.
 class ScrumStore: ObservableObject { // ObservableObject is a class-constrained protocol for connecting external model data to SwiftUI views.
     @Published var scrums: [DailyScrum] = []
     
@@ -16,5 +17,18 @@ class ScrumStore: ObservableObject { // ObservableObject is a class-constrained 
                                     appropriateFor: nil,
                                     create: false) // Use the shared instance of the FileManager class to get the location of the Documents directory for the current user.
         .appendingPathComponent("scrums.data") //  Call it to return the URL of a file named scrums.data.
+    }
+    
+    func load() async throws { // If an asynchronous function also throws an error, the async keyword comes before the throws keyword.
+        let task = Task<[DailyScrum], Error> { // Store the task in a let constant so that later app can access values returned or catch errors thrown from the task. The parameters tell the compiler that your closure returns [DailyScrum] and can throw an Error.
+            let fileURL = try Self.fileURL() // Use a guard statement to optionally load the file data.
+            guard let data = try? Data(contentsOf: fileURL) else {
+                return [] // Because the scrums.data file doesn’t exist at the beginning, so return an empty array if there’s an error reading the file.
+            }
+            let dailyScrums = try JSONDecoder().decode([DailyScrum].self, from: data)
+            return dailyScrums // The value returned from the task closure is available when the task completes. Returning the array fixes the compiler error.
+        }
+        let scrums = try await task.value // `try await` to wait for the task to finish and assign the value to a constant named `scrums`. If the JSONDecoder throws an error inside the task, the error will be propagated when app tries to access the value property
+        self.scrums = scrums // The type of task.value is the type that defined in the task initializer: [DailyScrum].
     }
 }
