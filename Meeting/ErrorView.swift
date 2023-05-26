1d78f5dc58d2892bed2baaf034c342d0e2fd679b
Teff <110036529+runnneverstop722@users.noreply.github.com>
2023-05-26 12:47:12 +0900

    - Report errors (Display errors when the unexpected happens) : Use data bindings to trigger a modal presentation of an error view. The user dismisses the modal view to continue using the app.

--- Meeting/ErrorView.swift
+++ Meeting/ErrorView.swift
@@ -9,22 +9,32 @@ import SwiftUI
 
 struct ErrorView: View {
     let errorWrapper: ErrorWrapper // Add a constant ErrorWrapper property.
+    @Environment(\.dismiss) private var dismiss // With the @Environment property wrapper, we can read a value that the view’s environment stores, such as the view’s presentation mode, scene phase, visibility, or color scheme. In this case, we’ll access the view’s dismiss structure and call it like a function to dismiss the view.
     
     var body: some View {
-        VStack {
-            Text("An error has occurred")
-                .font(.title)
-                .padding(.bottom)
-            Text(errorWrapper.error.localizedDescription) // Error provides a localized string description. If the error’s user info dictionary doesn’t provide a value for the description key, the system constructs a localized string from the domain and code.
-                .font(.headline)
-            Text(errorWrapper.guidance)
-                .font(.caption)
-                .padding(.top)
-            Spacer()
-        }
-        .padding()
-        .background(.ultraThinMaterial)
+        NavigationStack {
+            VStack {
+                Text("An error has occurred")
+                    .font(.title)
+                    .padding(.bottom)
+                Text(errorWrapper.error.localizedDescription) // Error provides a localized string description. If the error’s user info dictionary doesn’t provide a value for the description key, the system constructs a localized string from the domain and code.
+                    .font(.headline)
+                Text(errorWrapper.guidance)
+                    .font(.caption)
+                    .padding(.top)
+                Spacer()
+            }
+            .padding()
+            .background(.ultraThinMaterial)
         .cornerRadius(16)
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) { // The Dismiss button will be on the trailing side of the user interface.
+                Button("dismiss") {
+                    dismiss()
+                }
+            }
+        }
     }
 }
 
--- Meeting/MeetingApp.swift
+++ Meeting/MeetingApp.swift
@@ -10,6 +10,7 @@ import SwiftUI
 @main
 struct MeetingApp: App {
     @StateObject private var store = ScrumStore() // The @StateObject property wrapper creates a single instance of an observable object for each instance of the structure that declares it.
+    @State private var errorWrapper: ErrorWrapper? // Add an optional state variable named errorWrapper. The default value of an optional is nil. When assigning a value to this state variable, SwiftUI updates the view.
     
     var body: some Scene {
         WindowGroup {
@@ -18,7 +19,8 @@ struct MeetingApp: App {
                     do { // Add a do-catch block to save the scrum store or halt execution if save() throws an error. Currently, the app terminates if it encounters an error writing to the file system. Need to add more robust error handling in a later tutorial
                         try await store.save(scrums: store.scrums)
                     } catch {
-                        fatalError(error.localizedDescription)
+                        errorWrapper = ErrorWrapper(error: error,
+                                                    guidance: "Try again later.")
                     }
                 }
             }
@@ -26,8 +28,14 @@ struct MeetingApp: App {
                     do { // Use a do-catch statement to load the saved scrum or halt execution if load() throws an error.
                         try await store.load()
                     } catch {
-                        fatalError(error.localizedDescription)
+                        errorWrapper = ErrorWrapper(error: error,
+                                                    guidance: "The app will load sample data and continue.")
                     }
+                }
+                .sheet(item: $errorWrapper) { // Add a sheet with a binding to the error wrapper item. The modal sheet provides a closure to execute code when the user dismisses the modal sheet, and a closure to supply a view to present modally.
+                    store.scrums = DailyScrum.sampleData // In the onDismiss closure, assign sample data to the scrums array. Load sample data when the user dismisses the modal. Because we provide a binding to the condition that initiates the presentation, SwiftUI resets the optional error wrapper to nil when the user dismisses the presentation.
+                } content: { wrapper in
+                    ErrorView(errorWrapper: wrapper) // Create an ErrorView that passes in the errorWrapper. The ErrorView displays when an error occurs.
                 }
         }
     }
